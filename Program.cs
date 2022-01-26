9aea9b232b27b6558aa9761fcfbb7862e13913e4
Derek Linden <dlinden2024@chadwickschool.org>
2022-01-25 21:51:20 -0800

    chore: Initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonalityQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
